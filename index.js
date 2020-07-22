ad3e8972ac4059a947faaa2129f6328de30f7982
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-22 02:49:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,11 +60,6 @@ function createRock(x) {
   return rock
 }
 
-/* End the game by clearing `gameInterval`,
- * removing all ROCKS from the DOM,
- * and removing the `moveDodger` event listener.
- * Finally, alert "YOU LOSE!" to the player.
- */
 function endGame() {
   gameInterval = null;
   ROCKS.forEach(rock =>{rock.remove()})
