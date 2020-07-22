d023047c695d16d9aa1c7cfabdbddb9de8a9faaa
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-22 00:28:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ var gameInterval = null
  * but all of your work should happen below.
  */
 
-var width = parseInt($('#dodger').css("width").replace('px',''), 10);
+//var width = parseInt($('#dodger').css("width").replace('px',''), 10);
 var leftEdge = parseInt(dodger.style.left.replace('px',''), 10)
 var rightEdge = leftEdge + width;
-var rightFrame = parseInt($("#game").css("width").replace('px',''), 10);
+//var rightFrame = parseInt($("#game").css("width").replace('px',''), 10);
 
 function checkCollision(rock) {
   // implement me!
